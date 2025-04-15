fc3184d14050052f96660422a67aad388c693fc7
SheepFJ <xiaoenshixiaoyang@gmail.com>
2025-04-15 21:39:43 +0800

    VidSheep2.0

--- VidSheep/main/main.js
+++ VidSheep/main/main.js
@@ -6,12 +6,12 @@
 TG频道：https://t.me/sheep_007xiaoyang
 GitHub：https://github.com/SheepFJ/QuantumultX
 脚本说明：用于PKC插件的视频与文本接口
-============ Quantumult X ============
+================ Loon==============
 
-[rewrite_local]
-^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/(?:zhanshi\/sheep_vod_info_(\d+)|videoword\/([^\/]+)|) url script-response-body https://raw.githubusercontent.com/SheepFJ/VidSheep/main/VidSheep/main/mainlogic.js
+[Script]
+http-response ^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/?$ script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/mainlogic.js,requires-body=false,tag=VidSheep
+http-response ^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/(api|userinfo\/username\/([^\/]+)|videoword\/([^\/]+)\/\?wd=([^\/]+)|videolist\/([^\/]+)) script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/api.js,requires-body=false,tag=VidSheepApi
 [mitm]
 hostname = api.sheep.com
 
 *************************************/
-
--- VidSheep/main/main_loon.js
+++ VidSheep/main/main_loon.js
@@ -9,9 +9,9 @@ GitHub：https://github.com/SheepFJ/QuantumultX
 ================ Loon==============
 
 [Script]
-http-response ^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/(?:zhanshi\/sheep_vod_info_(\d+)|videoword\/([^\/]+)|) script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep/main/VidSheep/main/mainlogic.js,requires-body=false,tag=VidSheep
+http-response ^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/?$ script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/mainlogic.js,requires-body=false,tag=VidSheep
+http-response ^https:\/\/api\.sheep\.com\/sheep\/videoPolymerization\/(api|userinfo\/username\/([^\/]+)|videoword\/([^\/]+)\/\?wd=([^\/]+)|videolist\/([^\/]+)) script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep69/refs/heads/main/main/api.js,requires-body=false,tag=VidSheepApi
 [mitm]
 hostname = api.sheep.com
 
 *************************************/
-
