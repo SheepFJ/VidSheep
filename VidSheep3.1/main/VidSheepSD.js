c7e5f936d1844d8eceee37732778c682ae77ef63
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-06-07 18:31:21 +0800

    Add files via upload

---
+++ VidSheep3.1/main/VidSheepSD.js
@@ -0,0 +1,8 @@
+#!url=https://github.com/SheepFJ/VidSheep
+#!name=VidSheep3.0 
+#!desc=VidSheep
+[MITM]
+hostname =% APPEND % api.sheep.com
+
+[Script]
+Rewrite: VidSheep3.0 = type = http - response, pattern = https ?: \/\/api\.sheep\.com\/sheep\/VidSheep\/,script-path= https:/ / raw.githubusercontent.com / SheepFJ / VidSheep / refs / heads / main / VidSheep3.1 / main / mainlogic.js, requires - body=true, timeout = 120
