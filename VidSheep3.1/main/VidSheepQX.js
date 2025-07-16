f157bfae404456a74ed865a6998ddfa32fd02fcc
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-07-16 22:24:29 +0800

    Update VidSheepQX.js

--- VidSheep3.1/main/VidSheepQX.js
+++ VidSheep3.1/main/VidSheepQX.js
@@ -9,7 +9,7 @@ GitHub：https://github.com/SheepFJ/VidSheep
 ============ Quantumult X ============
 
 [rewrite_local]
-^https?:\/\/www\.360\.cn\/sheep\/VidSheep\/ url script-response-body https://raw.githubusercontent.com/SheepFJ/VidSheep/refs/heads/main/VidSheep3.1/main/api.js
+^http?:\/\/www\.360\.cn\/sheep\/VidSheep\/ url script-response-body https://raw.githubusercontent.com/SheepFJ/VidSheep/refs/heads/main/VidSheep3.1/main/api.js
 [mitm]
 hostname = www.360.cn
 
