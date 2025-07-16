6d7bcd7f8f4692a9e25c29926970b62f3d6ec7ca
SheepFJ <125231595+SheepFJ@users.noreply.github.com>
2025-07-16 21:21:03 +0800

    Update VidSheepLoon.js

--- VidSheep3.1/main/VidSheepLoon.js
+++ VidSheep3.1/main/VidSheepLoon.js
@@ -9,7 +9,7 @@ GitHub：https://github.com/SheepFJ/VidSheep
 ================ Loon==============
 
 [Script]
-http-response ^https:\/\/www\.360\.cn\/sheep\/VidSheep\/ script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep/refs/heads/main/VidSheep3.1/main/api.js,requires-body=false,tag=VidSheep3.0
+http-response ^https?:\/\/www\.360\.cn\/sheep\/VidSheep\/ script-path=https://raw.githubusercontent.com/SheepFJ/VidSheep/refs/heads/main/VidSheep3.1/main/api.js,requires-body=false,tag=VidSheep3.0
 [mitm]
 hostname = www.360.cn
 
